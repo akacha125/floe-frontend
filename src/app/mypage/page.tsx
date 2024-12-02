a4c013b68e1f10461cba582a6a2cee598861828f
JaeHunKang <dawin00k@gmail.com>
2024-12-03 00:18:29 +0900

    Info & Tab Navigation 구현 중

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -6,10 +6,16 @@ import styles from './MyPage.module.css';
 import Header from '../header/page';
 import NavBar from '../navBar/page';
 
+import TabNavigation from '@/components/tabNavigation/page';
+
 export default function MyPage() {
   // Image 변경 변수
   const fileInputRef = useRef<HTMLInputElement>(null);
 
+  // Tab Navigation 선택 변수
+  const [activeTab, setActivetab] = useState('POSTS');
+
+  // 현재 이미지를 클릭했을때
   const handleImageClick = () => {
     if (fileInputRef.current) {
       fileInputRef.current.click();
@@ -32,6 +38,25 @@ export default function MyPage() {
     }
   };
 
+  // Tab 선택했을 때
+  const handleTabClick = (tab: string) => {
+    setActivetab(tab);
+  };
+
+  const TabContent = () => {
+    switch (activeTab) {
+      case 'POSTS':
+        return <div> POSTS Contents </div>;
+      case 'LIKE':
+        return <div> LIKE Contents </div>;
+      case 'SAVE':
+        return <div> SAVE Contents </div>;
+      default:
+        return <div> Default Contents </div>;
+    }
+  };
+
+  // HTML
   return (
     <>
       <Header />
@@ -41,16 +66,19 @@ export default function MyPage() {
         </aside>
         {/* 
           - Profile 영역 
-            1. Image
-            2. posts
-            3. followers
-            4. following
-            5. Setting
-            6. Edit Profile
-            7. User Name
+            1. Image ✅
+            2. posts ✅
+            3. followers ✅
+            4. following ✅
+            5. Setting ✅
+            6. Edit Profile ✅
+            7. User Name ✅
         */}
-        <div>
-          <div className={styles['user-Image']} onClick={handleImageClick}></div>
+        <div className={styles['user-Contents']}>
+        <div className={styles['user-Info']}>
+          <div
+            className={styles['user-Image']}
+            onClick={handleImageClick}></div>
           <input
             type="file"
             accept="image/*"
@@ -58,6 +86,30 @@ export default function MyPage() {
             onChange={handleFileChange}
             style={{ display: 'none' }}
           />
+          <div className={styles['user-Data-Wrapper']}>
+            <div className={styles['user-Data']}>
+              <div className={styles['user-Posts']}>
+                <div className={styles['data-Number']}>10</div>
+                <div>posts</div>
+              </div>
+
+              <div className={styles['user-Followers']}>
+                <div className={styles['data-Number']}>10</div>
+                <div>followers</div>
+              </div>
+
+              <div className={styles['user-Following']}>
+                <div className={styles['data-Number']}>10</div>
+                <div>following</div>
+              </div>
+            </div>
+
+            <div className={styles['user-Edit']}>
+              <button className={styles['user-setting']} />
+              <button className={styles['edit-Button']}>Edit profile</button>
+              <div className={styles['user-Name']}>GANG JAEHUN</div>
+            </div>
+          </div>
         </div>
 
         {/* 
@@ -66,12 +118,19 @@ export default function MyPage() {
             2. LIKE
             3. SAVE
         */}
+        <div className={styles['tab-Content']}>
+          <TabNavigation activeTab={activeTab} onTabClick={handleTabClick} />
+          <div>{TabContent()}</div>
+        </div>
+
         {/*
           - Tab Bar에 따른 Show Components 영역
             1. 올린 게시물
             2. 좋아요 누른 게시물
             3. 저장한 게시물
         */}
+        </div>
+        
       </div>
     </>
   );
---
+++ src/components/tabNavigation/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React, { useState } from 'react';
+import styles from './Tab.Navigation.module.css';
+
+// Tab Navigation Interface
+interface TabNavigationProps {
+  activeTab: string;
+  onTabClick: (tab: string) => void;
+}
+
+// Tab Props를 받음
+export default function TabNavigation({
+  activeTab,
+  onTabClick,
+}: TabNavigationProps) {666666
+  const tabs: Array<string> = ['POSTS', 'LIKE', 'SAVE'];
+
+  return (
+    <div className={styles['tab-Container']}>
+      {tabs.map((tab) => (
+        <button
+          className={`
+            ${styles['tab-Button']}
+            ${activeTab === tab ? styles.active: ''}
+            `}
+          key={tab}
+          onClick={() => onTabClick(tab)}>
+          {tab}
+        </button>
+      ))}
+    </div>
+  );
+}
