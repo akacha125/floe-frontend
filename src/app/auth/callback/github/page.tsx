3f91f58e0505d4bbf659a82ce42348a82b032588
JaeHunKang <dawin00k@gmail.com>
2024-12-15 03:02:19 +0900

    중간 내용 저장

---
+++ src/app/auth/callback/github/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import styles from './Github.Callback.module.css';
+
+export default function GithubCallback() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+    const code = query.get('code'); 
+
+    if (code) {
+      console.log('Github Authorization Code:', code);
+
+      router.push('/')
+    } else {
+      console.error('Authorization Code가 없습니다.');
+    }
+  }, []);
+
+  return (
+    <div className={styles['github-loading']}>
+      Google 로그인 처리 중...
+    </div>
+  );
+}
